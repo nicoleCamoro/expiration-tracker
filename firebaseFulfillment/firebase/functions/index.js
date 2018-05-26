ddb260da9198b4de1f0ae2a5c5849c6425f766df
Franchette <franchette@sym.ph>
2018-05-26 19:03:43 +0800

    initial commit

---
+++ firebaseFulfillment/firebase/functions/index.js
@@ -0,0 +1,128 @@
+'use strict';
+
+const functions = require('firebase-functions');
+const DialogflowApp = require('actions-on-google').DialogflowApp;
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+  if (request.body.queryResult) {
+    processV2Request(request, response);
+  } else {
+    console.log('Invalid Request');
+    return response.status(400).end('Invalid Webhook Request');
+  }
+});
+
+
+function processV2Request(request, response) {
+  let action = (request.body.queryResult.action) ? request.body.queryResult.action : 'default';
+  let inputContexts = request.body.queryResult.contexts; 
+  let requestSource = (request.body.originalDetectIntentRequest) ? request.body.originalDetectIntentRequest.source : undefined;
+  let session = (request.body.session) ? request.body.session : undefined;
+  
+  const actionHandlers = {
+    'input.welcome': () => {
+      sendResponse('Hello, Welcome to my Dialogflow agent!');
+    },
+    'input.unknown': () => {
+      sendResponse('I\'m having trouble, can you try that again?');
+    },
+    'default': () => {
+      let responseToUser = {
+        //fulfillmentMessages: richResponsesV2, // Optional, uncomment to enable
+        //outputContexts: [{ 'name': `${session}/contexts/weather`, 'lifespanCount': 2, 'parameters': {'city': 'Rome'} }], // Optional, uncomment to enable
+        fulfillmentText: 'This is from Dialogflow\'s Cloud Functions for Firebase editor! :-)' // displayed response
+      };
+      sendResponse(responseToUser);
+    },
+    'new_expire': () => {
+      let responseToUser = {
+        // save to database
+        fulfillmentText: 'Adding new item to database'
+      };
+      sendResponse(responseToUser);
+    }
+  };
+  // If undefined or unknown action use the default handler
+  if (! [action]) {
+    action = 'default';
+  }
+  // Run the proper handler function to handle the request from Dialogflow
+  actionHandlers[action](); 
+  // Function to send correctly formatted responses to Dialogflow which are then sent to the user
+  function sendResponse(responseToUser) {
+    // if the response is a string send it as a response to the user
+    if (typeof responseToUser === 'string') {
+      let responseJson = { fulfillmentText: responseToUser }; // displayed response
+      response.json(responseJson); // Send response to Dialogflow
+    } else {
+      // If the response to the user includes rich responses or contexts send them to Dialogflow
+      let responseJson = {};
+      // Define the text response
+      responseJson.fulfillmentText = responseToUser.fulfillmentText;
+      // Optional: add rich messages for integrations (https://dialogflow.com/docs/rich-messages)
+      if (responseToUser.fulfillmentMessages) {
+        responseJson.fulfillmentMessages = responseToUser.fulfillmentMessages;
+      }
+      // Optional: add contexts (https://dialogflow.com/docs/contexts)
+      if (responseToUser.outputContexts) {
+        responseJson.outputContexts = responseToUser.outputContexts;
+      }
+      // Send the response to Dialogflow
+      console.log('Response to Dialogflow: ' + JSON.stringify(responseJson));
+      response.json(responseJson);
+    }
+  }
+}
+const richResponseV2Card = {
+  'title': 'Title: this is a title',
+  'subtitle': 'This is an subtitle.  Text can include unicode characters including emoji 📱.',
+  'imageUri': 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+  'buttons': [
+    {
+      'text': 'This is a button',
+      'postback': 'https://assistant.google.com/'
+    }
+  ]
+};
+const richResponsesV2 = [
+  {
+    'platform': 'ACTIONS_ON_GOOGLE',
+    'simple_responses': {
+      'simple_responses': [
+        {
+          'text_to_speech': 'Spoken simple response',
+          'display_text': 'Displayed simple response'
+        }
+      ]
+    }
+  },
+  {
+    'platform': 'ACTIONS_ON_GOOGLE',
+    'basic_card': {
+      'title': 'Title: this is a title',
+      'subtitle': 'This is an subtitle.',
+      'formatted_text': 'Body text can include unicode characters including emoji 📱.',
+      'image': {
+        'image_uri': 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png'
+      },
+      'buttons': [
+        {
+          'title': 'This is a button',
+          'open_uri_action': {
+            'uri': 'https://assistant.google.com/'
+          }
+        }
+      ]
+    }
+  },
+  {
+    'platform': 'FACEBOOK',
+    'card': richResponseV2Card
+  },
+  {
+    'platform': 'SLACK',
+    'card': richResponseV2Card
+  }
+];
